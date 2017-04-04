4351d2847052a536e2a242ac771ea707f508c2dd
Sohani Rao <sohanirao@google.com>
2017-04-03 17:48:32 -0700

    Add WiFi offload HAL implementation for V1.0 interface.
    Bug: 32842314
    Test: Unit tests and mannual test to ensure service
          is running.
    Change-Id: I9612ec28b9b042be10b7e022362124de906649ab

--- hidl.mk
+++ hidl.mk
@@ -119,6 +119,6 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service
-
+    android.hardware.wifi@1.0-service \
+    android.hardware.wifi.offload@1.0-service
 
