4b9c46885d8ae291874913039dfb87bea7f24b14
Alex Koskovich <zvnexus@outlook.com>
2019-10-15 16:20:54 -0400

    op3: Don't build QTI Power HAL.
    Change-Id: I415880f6e6111f5b1b514b537a777e7c65fcbdfa

--- device.mk
+++ device.mk
@@ -381,10 +381,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
