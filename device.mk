b5ed81067854fac6027539275c81a6d64b43fa43
Alex Koskovich <zvnexus@outlook.com>
2019-10-16 15:35:47 -0400

    Remove boot animation definitions.
    Defined in vendor.
    
    Change-Id: I285d0f04ef8c62318d5c6eb673a2747247039bbe

--- device.mk
+++ device.mk
@@ -80,10 +80,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m \
